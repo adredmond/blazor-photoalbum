b4cdba1214e608d7bfbf54e5d4f9555f04174446
Andrew Redmond <adredmond@gmail.com>
2023-03-24 12:50:44 -0400

    basic layout and components

---
+++ Photos/Photo.cs
@@ -0,0 +1,9 @@
+﻿namespace blazor_photoalbum.Photos;
+public class Photo
+{
+  public int AlbumId { get; set; }
+  public int Id { get; set; }
+  public string Title { get; set; }
+  public string Url { get; set; }
+  public string ThumbnailUrl { get; set; }
+}
